d940eccd9a44d36d298f1b4933aa1c1bbe434667
AnneOmberg <111729606+AnneOmberg@users.noreply.github.com>
2024-01-23 03:21:18 +0100

    deleksasmen 2
    qwerty

---
+++ script.js
@@ -0,0 +1,8 @@
+let titleHTML = "<h2>HTML</h2>"
+
+resources.map(title => titleHTML += `
+<ul><li><button><h2>${title.category}</h2></button></li></ul>`)
+
+document.getElementsByTagName("header") =
+
+console.log(titleHTML)
